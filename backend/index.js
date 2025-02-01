b89e8a2ee356c36b9ddb58f34b49dd9daf471d8f
kesava049 <kalepallikesavulareddy@gmail.com>
2025-02-01 18:42:42 +0530

    ignored_modules

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 app.use(express.json());
-app.use(bodyParser.json())
-
+app.use(bodyParser.json());
+const zod = require('zod');
+const {createTodo, updateTodo} = require('..types.js')
 
 
 app.post('/todo',(req,res)=>{
-
+    const response = createTodo(req.body);
+    if(!response.success){
+        res.json({
+            msg: "Invalid inputs!"
+        })
+    }
 })
 
 app.get('/todos',(req,res)=>{
-
+    const response = updateTodo(req.body);
+    if(!response.success){
+        res.json({
+            msg: "Invalid inputs!"
+        })
+    }updateTodo
 })
 
 app.put('/completed',(req,res)=>{
